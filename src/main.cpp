fdc598eaa07d63c35f9c4fdc94e664aed1ad96c6
Aiden Pham <quangpham.245@proton.me>
2026-01-17 22:33:11 +0700

    GLFW: add GLFW

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,31 @@
 #include<iostream>
 
+#include <GLFW/glfw3.h>
+
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+	std::cout << "Hello Wolrd" << std::endl;
+	GLFWwindow* window;
+
+	if (!glfwInit()) {
+		std::cerr << "Failed to initialize GLFW" << std::endl;
+		return -1;
+	}
+
+	window = glfwCreateWindow(800, 600, "OpenGL Window", nullptr, nullptr);
+	if (!window) {
+		std::cerr << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
 }
