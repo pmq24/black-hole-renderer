46969443f2cb3378738b8d1d543773e3e1d1811e
Aiden Pham <quangpham.245@proton.me>
2026-01-17 23:18:39 +0700

    Formating: set up Clang Format and Lefthook

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,30 @@
-#include<iostream>
+#include <iostream>
 
 #include <GLFW/glfw3.h>
 
 int main() {
-	std::cout << "Hello Wolrd" << std::endl;
-	GLFWwindow* window;
+  std::cout << "Hello Wolrd" << std::endl;
+  GLFWwindow *window;
 
-	if (!glfwInit()) {
-		std::cerr << "Failed to initialize GLFW" << std::endl;
-		return -1;
-	}
+  if (!glfwInit()) {
+    std::cerr << "Failed to initialize GLFW" << std::endl;
+    return -1;
+  }
 
-	window = glfwCreateWindow(800, 600, "OpenGL Window", nullptr, nullptr);
-	if (!window) {
-		std::cerr << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-		return -1;
-	}
+  window = glfwCreateWindow(800, 600, "OpenGL Window", nullptr, nullptr);
+  if (!window) {
+    std::cerr << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
 
-	glfwMakeContextCurrent(window);
+  glfwMakeContextCurrent(window);
 
-	while (!glfwWindowShouldClose(window))
-	{
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
+  while (!glfwWindowShouldClose(window)) {
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
 
-	glfwTerminate();
-	return 0;
+  glfwTerminate();
+  return 0;
 }
